a224df2c3fc2df0de149bd29a1771ef4028e16b7
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-08-26 18:45:05 -0300

    v1.0

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 2 - Faça um programa que exiba Olá usuário, eu me chamo Ermogenes! (utilize o seu nome).
+
+            Console.Write("Digite seu nome: ");
+            string userName = Console.ReadLine(); // Armazena o nome do usuário que está operando o programa.
+
+            Console.WriteLine($"Olá {userName}, eu me chamo Leandro!"); // Resolução da tarefa.
+
+            Console.Write("Pressione qualquer tecla para fechar. . . "); // Aguarda o usuário fechar o programa ou inserir uma tecla.
+            Console.ReadKey();
+        }
+    }
+}
